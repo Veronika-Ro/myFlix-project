fe8a040c7bd1ed207c74226cfdbb2063e320699c
Veronika-Ro <79661110+Veronika-Ro@users.noreply.github.com>
2021-05-03 20:07:50 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,38 +1,82 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 const app = express();
 
-let topMovies = [
+app.use(bodyParser.json());
+
+let movies = [
     {
-        title: 'Harry Potter and the Philosopher\'s Stone',
+        name: 'Harry Potter and the Philosopher\'s Stone',
         studio: 'Warner Brothers'
     },
     {
-        title: 'Lord of the Rings',
+        name: 'Lord of the Rings',
         studio: 'New Line Cinema'
     },
     {
-        title: 'Frozen',
+        name:'Frozen',
         studio: 'Disney'
     },
     {
-        title: 'Star Wars',
+        name: 'Star Wars',
         studio: 'Lucas Films'
     },
     {
-        title: 'Avengers',
+        name: 'Avengers',
         studio: 'Marvel'
     }
   ];
   
-  // GET requests
+  // GET requests for all movies
   app.get('/', (req, res) => {
     res.send('Welcome to the best Movie Collection!');
   });
   
   app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.json(movies);
   });
+
+  // GET requests for a specific movie
+  app.get('/movies/:name', (req, res) => {
+    res.json(movies.find((movies) =>
+      { return movies.name === req.params.name }));
+      // add error handler fo rmovies not found
+  });
+
+  // GET requests to be updated for genres and directors
+
+  app.get('/genres', (req, res) => {
+    res.send('Successful GET request returning data on all genres');
+  });
+
+  app.get('/directors', (req, res) => {
+    res.send('Successful GET request returning data on all directors');
+  });
+ 
+  // POST requests to be updated 
+  app.post('/users/:username', (req, res) => {
+    res.send('Successful POST request adding a new user');;
+  });
+
+  app.post("/users/:username/favorites",(req,res)=>{
+    res.send("movie added to favorites");
+  });
+
+// PUT requests to be updated 
+  app.put('/users/:username', (req, res) => {
+    res.send('username has been updated');;
+} );
+
+// DELETE requests to be updated 
+  app.delete("/users/:username",(req,res)=>{
+    res.send("user was deleted")
+  });
+
+  app.delete("/users/:username/favorites/:name",(req,res)=>{
+    res.send("movie as removed from favorites")
+});
 
   //Return the documentation html
 
